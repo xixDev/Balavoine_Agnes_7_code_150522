f0d1d2511e645ed623a562ece94409caf872ff71
xix <94467370+xixDev@users.noreply.github.com>
2022-06-15 12:30:55 +0200

    Delete front/src directory
    Update structure folders

--- front/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-// import axios from 'axios';
-// import VueAxios from 'vue-axios';
-
-// createApp(App).use(VueAxios, axios, router).mount('#app');
-createApp(App).use(router).mount('#app');
